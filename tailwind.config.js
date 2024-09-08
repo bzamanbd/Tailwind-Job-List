4bb0b00e366c4f08f7a4704936e78c749feaa13b
bzamanbd <riponzamanbd@gmail.com>
2024-09-08 07:01:27 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{ 
+        primaryColor:"#0f172a ",
+        secondaryColor:"#64BABB",
+      }
+    },
+  },
+  plugins: [],
+}
+
